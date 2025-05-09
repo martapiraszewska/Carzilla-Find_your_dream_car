5ff4d693a180a748b6d4d8c50d3e914945bac0d1
Michał Bloch <112960926+Dnafivuq@users.noreply.github.com>
2025-05-10 00:54:22 +0200

    frontend backend demo integration with search button, added proxy to skip CORS issues

--- frontend/src/elements/SearchBar.js
+++ frontend/src/elements/SearchBar.js
@@ -3,6 +3,19 @@ import './SearchBar.css';
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
+  const [queryData, setData] = useState([]);  // not ideal way to do this
+
+  function querySearch(){
+    fetch(`search?${query}`)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => console.error('Error:', error));
+  }
 
   return (
     <div className="search-bar">
@@ -10,10 +23,11 @@ const SearchBar = () => {
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search..."
+        placeholder="Search... can be empty"
         className="search-input"
       />
-      <button className="search-button">Search</button>
+      <button className="search-button" onClick={querySearch}> Search</button>
+      {queryData.map(d => ` |${d.brand}|`)}  {/*another not good thing*/}
     </div>
   );
 };
