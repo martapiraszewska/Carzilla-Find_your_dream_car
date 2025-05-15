d52525b954a00e6797d6c695f9850d356b5abfca
jbaginsk <01187206@pw.edu.pl>
2025-05-15 14:58:44 +0200

    changed incoherent names in database

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,16 +1,21 @@
 from .db import db
 from flask_login import UserMixin
 
+
 class CarCondition(db.Model):
     __tablename__ = "Car_condition"
-    Condition_ID = db.Column(db.Integer, primary_key=True)
+    Car_Condition_ID = db.Column(db.Integer, primary_key=True)
     Condition = db.Column(db.String, nullable=False)
+
 
 class CarDealer(db.Model):
     __tablename__ = "Car_dealer"
     Car_dealer_ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String, nullable=False)
-    Address_ID = db.Column(db.Integer, db.ForeignKey("Address.Address_ID"), nullable=False)
+    Address_ID = db.Column(
+        db.Integer, db.ForeignKey("Address.Address_ID"), nullable=False
+    )
+
 
 class Address(db.Model):
     __tablename__ = "Address"
@@ -20,11 +25,13 @@ class Address(db.Model):
     Street_number = db.Column(db.Integer, nullable=False)
     City_ID = db.Column(db.Integer, db.ForeignKey("City.City_ID"), nullable=False)
 
+
 class City(db.Model):
     __tablename__ = "City"
     City_ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String, nullable=False)
     Country = db.Column(db.String, nullable=False)
+
 
 class Car(db.Model):
     __tablename__ = "Car"
@@ -34,8 +41,13 @@ class Car(db.Model):
     Color = db.Column(db.String, nullable=False)
     Mileage = db.Column(db.Integer, nullable=False)
     Price = db.Column(db.Integer, nullable=False)
-    Condition_ID = db.Column(db.Integer, db.ForeignKey("Car_condition.Condition_ID"), nullable=False)
-    Dealer_ID = db.Column(db.Integer, db.ForeignKey("Car_dealer.Car_dealer_ID"), nullable=False)
+    Car_Condition_ID = db.Column(
+        db.Integer, db.ForeignKey("Car_condition.Car_Condition_ID"), nullable=False
+    )
+    Car_Dealer_ID = db.Column(
+        db.Integer, db.ForeignKey("Car_dealer.Car_dealer_ID"), nullable=False
+    )
+
 
 class Client(db.Model):
     __tablename__ = "Client"
@@ -46,16 +58,19 @@ class Client(db.Model):
     Mail = db.Column(db.String, nullable=False)
     Phone = db.Column(db.String, nullable=False)
 
+
 class EmployeeStatus(db.Model):
     __tablename__ = "Employee_status"
     Employee_status_ID = db.Column(db.Integer, primary_key=True)
     Status_name = db.Column(db.String, nullable=False)
 
+
 class LoginCredentials(db.Model):
-    __tablename__ = 'Login_credentials'
+    __tablename__ = "Login_credentials"
     Login_credentials_ID = db.Column(db.Integer, primary_key=True)
     Login = db.Column(db.String, nullable=False)
     Password = db.Column(db.String, nullable=False)
+
 
 class Employee(UserMixin, db.Model):
     __tablename__ = "Employee"
@@ -66,13 +81,22 @@ class Employee(UserMixin, db.Model):
     Salary = db.Column(db.Integer)
     Date_of_birth = db.Column(db.Date, nullable=False)
     Phone_number = db.Column(db.String)
-    Employee_status_ID = db.Column(db.Integer, db.ForeignKey("Employee_status.Employee_status_ID"), nullable=False)
-    Car_dealer_ID = db.Column(db.Integer, db.ForeignKey("Car_dealer.Car_dealer_ID"), nullable=False)
-    Login_credentials_ID = db.Column(db.Integer, db.ForeignKey('Login_credentials.Login_credentials_ID'), nullable=False)
+    Employee_status_ID = db.Column(
+        db.Integer, db.ForeignKey("Employee_status.Employee_status_ID"), nullable=False
+    )
+    Car_dealer_ID = db.Column(
+        db.Integer, db.ForeignKey("Car_dealer.Car_dealer_ID"), nullable=False
+    )
+    Login_credentials_ID = db.Column(
+        db.Integer,
+        db.ForeignKey("Login_credentials.Login_credentials_ID"),
+        nullable=False,
+    )
 
     # flask_login
     def get_id(self):
         return int(self.Employee_ID)
+
 
 class Position(db.Model):
     __tablename__ = "Position"
@@ -81,18 +105,25 @@ class Position(db.Model):
     Min_salary = db.Column(db.Integer)
     Max_salary = db.Column(db.Integer)
 
+
 class PositionHistory(db.Model):
     __tablename__ = "Position_history"
     Position_history_ID = db.Column(db.Integer, primary_key=True)
     Date_start = db.Column(db.Date, nullable=False)
     Date_end = db.Column(db.Date)
-    Position_ID = db.Column(db.Integer, db.ForeignKey("Position.Position_ID"), nullable=False)
-    Employee_ID = db.Column(db.Integer, db.ForeignKey("Employee.Employee_ID"), nullable=False)
+    Position_ID = db.Column(
+        db.Integer, db.ForeignKey("Position.Position_ID"), nullable=False
+    )
+    Employee_ID = db.Column(
+        db.Integer, db.ForeignKey("Employee.Employee_ID"), nullable=False
+    )
+
 
 class TransactionType(db.Model):
     __tablename__ = "Transaction_type"
     Transaction_type_ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String, nullable=False)
+
 
 class Invoice(db.Model):
     __tablename__ = "Invoice"
@@ -101,19 +132,29 @@ class Invoice(db.Model):
     Issue_date = db.Column(db.Date)
     NIP = db.Column(db.Integer, nullable=False)
 
+
 class Transaction(db.Model):
     __tablename__ = "Transaction"
     Transaction_ID = db.Column(db.Integer, primary_key=True)
     Date = db.Column(db.Date, nullable=False)
     Value = db.Column(db.Integer, nullable=False)
     Client_ID = db.Column(db.Integer, db.ForeignKey("Client.Client_ID"), nullable=False)
-    Employee_ID = db.Column(db.Integer, db.ForeignKey("Employee.Employee_ID"), nullable=False)
-    Transaction_type_ID = db.Column(db.Integer, db.ForeignKey("Transaction_type.Transaction_type_ID"), nullable=False)
+    Employee_ID = db.Column(
+        db.Integer, db.ForeignKey("Employee.Employee_ID"), nullable=False
+    )
+    Transaction_type_ID = db.Column(
+        db.Integer,
+        db.ForeignKey("Transaction_type.Transaction_type_ID"),
+        nullable=False,
+    )
     Invoice_ID = db.Column(db.Integer, db.ForeignKey("Invoice.Invoice_ID"), unique=True)
+
 
 class EmployeeStats(db.Model):
     __tablename__ = "Employee_stats"
     Year = db.Column(db.Integer, primary_key=True)
     Month = db.Column(db.Integer, primary_key=True)
-    Employee_ID = db.Column(db.Integer, db.ForeignKey("Employee.Employee_ID"), primary_key=True)
+    Employee_ID = db.Column(
+        db.Integer, db.ForeignKey("Employee.Employee_ID"), primary_key=True
+    )
     Sales_sum = db.Column(db.Integer, nullable=False)
