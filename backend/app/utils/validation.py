2d324417fe6900000abfa09ef228aea68f9f9a53
jbaginsk <01187206@pw.edu.pl>
2025-05-15 14:59:21 +0200

    more validation

--- backend/app/utils/validation.py
+++ backend/app/utils/validation.py
@@ -18,36 +18,38 @@ class Valid:
     def valid_presence(self, data: dict, required_fields: list):
         missing = [field for field in required_fields if field not in data]
         if missing:
-            self._is_valid = False
-            self._error_msg += f"Missing fields: {', '.join(missing)}. "
+            self._add_error(f"Missing fields: {', '.join(missing)}. ")
 
     def valid_phone_number(self, number):
         phone_pattern = r"^\+?\d[\d\s\-]{5,20}$"
         if not re.fullmatch(phone_pattern, number):
-            self._is_valid = False
-            self._error_msg += "Invalid phone number format. "
+            self._add_error("Invalid phone number format. ")
 
     def valid_date(self, date, format="%Y-%m-%d"):
         try:
             datetime.strptime(date, format).date()
         except ValueError:
-            self._is_valid = False
-            self._error_msg += "Invalid Date_of_birth format. "
+            self._add_error("Invalid Date_of_birth format. ")
 
     def valid_salary(self, salary, lower_bound=0, upper_bound=math.inf):
         try:
             salary = int(salary)
         except ValueError:
-            self._is_valid = False
-            self._error_msg += "Salary must be an integer. "
+            self._add_error("Salary must be an integer. ")
             return
 
         if salary < lower_bound:
-            self._is_valid = False
-            self._error_msg += "Salary too low. "
+            self._add_error("Salary too low. ")
         if salary > upper_bound:
-            self._is_valid = False
-            self._error_msg += "Salary too high. "
+            self._add_error("Salary too high. ")
+
+    def valid_number_positive(self, number):
+        try:
+            number = int(number)
+            if number < 0:
+                self._add_error("Number should not be negative. ")
+        except ValueError:
+            self._add_error("Number is not an integer. ")
 
     def valid_foreign_keys(self, data: dict):
         for field in data:
@@ -55,15 +57,13 @@ class Valid:
                 continue
 
             if not isinstance(data[field], int):
-                self._is_valid = False
-                self._error_msg += f"{field} must be an integer. "
+                self._add_error(f"{field} must be an integer. ")
                 continue
 
             try:
                 model_class_name = str_to_class(convert_case(field[:-3]))
             except Exception:
-                self._is_valid = False
-                self._error_msg += f"{field} does not refer to any table. "
+                self._add_error(f"{field} does not refer to any table. ")
                 continue
 
             id_found = False
@@ -72,10 +72,13 @@ class Valid:
                     id_found = True
 
             if not id_found:
-                self._is_valid = False
-                self._error_msg += (
+                self._add_error(
                     f"{model_class_name} does not contain ID={data[field]}. "
                 )
+
+    def _add_error(self, msg):
+        self._is_valid = False
+        self._error_msg += msg
 
 
 def str_to_class(classname):
